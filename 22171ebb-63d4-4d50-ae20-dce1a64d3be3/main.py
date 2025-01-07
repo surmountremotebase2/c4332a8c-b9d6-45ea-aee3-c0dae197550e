35d7b7402ae14a427ec77bf1fbe10203cc67c33f
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-01-07 02:11:21 -0500

    User Haris Javed saved changes to main.py at 01/07/2025, 07:11:20 UTC time.

--- 22171ebb-63d4-4d50-ae20-dce1a64d3be3/main.py
+++ 22171ebb-63d4-4d50-ae20-dce1a64d3be3/main.py
@@ -1,1 +1,33 @@
 #Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.logging import log
+
+from surmount.technical_indicators import RSI
+from surmount.data import Ratios
+
+class TradingStrategy(Strategy):
+
+    def __init__(self):
+        self.tickers = ("AAPL")
+        self.data_list = [Ratios(ticker) for ticker in self.tickers]
+
+    @property
+    def interval(self):
+        return "1hour"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+
+    def run(self, data):
+        allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
+        ohlcv = data.get("ohlcv")
+        ratios = data.get(("ratios","AAPL"))
+        log(str(ohlcv))
+        log(str(ratios))
+        # WRITE STRATEGY LOGIC HERE
+        return TargetAllocation(allocation_dict)
